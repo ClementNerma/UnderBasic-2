0073cdcac194a5f29f0d2fe1883b91b3182a63b3
Clément Nerma <clement.nerma@gmail.com>
2016-09-17 16:36:59 +0200

    Made "list" and "matrix" types dynamic

--- js/underbasic.js
+++ js/underbasic.js
@@ -54,7 +54,7 @@ const UnderBasic = (new (function() {
 
   /** The types that doesn't support any operation (static types)
     * @type {array} */
-  const staticTypes = [ "list", "matrix", "picture", "gdb", "program", "appvar", "group", "application" ];
+  const staticTypes = [ "picture", "gdb", "program", "appvar", "group", "application" ];
 
   /** The errors width
     * @type {number} */
